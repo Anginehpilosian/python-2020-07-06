49fc8eb732899e891376e226e47b5451874d2705
Chris Bautista <CBaut@users.noreply.github.com>
2020-07-06 14:55:44 -0700

    initial commit

---
+++ week1/day1/alogs.js
@@ -0,0 +1,25 @@
+/*
+  String: Reverse
+
+  Implement reverseString(str)
+  abc
+  cba
+*/
+
+/*
+  Acronyms
+
+  Create a function that, given a string, returns the string’s acronym
+  (first letter of each word capitalized).
+
+  Do it with .split first if you need to, then try to do it without
+*/
+
+/*
+  Case insensitive string comparison
+
+  const test1StrA = "ABC"
+  const test1StrB = "abc"
+  caseInsensitiveCompare(test1StrA, test1StrB) // Output: true
+
+*/
---
+++ week1/day1/intro.py
@@ -0,0 +1,73 @@
+# Intro
+
+# [x] shell and syntax
+# check python version
+# variables
+
+
+# Javascript
+# var variable = 'string'
+
+# [x] executing files
+# python3 intro.py
+print('Hellow!')
+
+# [x] comments
+# MacOS
+# cmd + /
+# Windows
+# ctrl + /
+
+
+# [ ] data types
+
+# # bool
+# True # true
+# False # false
+# # str
+# "string"
+# # int
+# 1
+# # float
+# 1.1
+# # (Tuple, Tuple)
+
+# # [List]
+# [1,2,3,"1","2"]
+
+# {Dict_key: Dict_val}
+# {key: value}
+
+# [x] strings
+
+
+# [ ] lists
+someList = [1, 2, 3, "1", "2", ('Sidney', 'Aus')]
+someList.append(3)
+someList.pop(0)
+print(someList)
+
+# print(someList.length)
+print(len(someList))
+# [ ] conditional expressions
+# cond = True
+# if (cond) {
+# some code.age = 15
+# }
+age = 55
+if age >= 55:
+    print('You get a discount')
+elif age >= 18:
+    print('Legal age')
+else:
+    print('You are so young!')
+
+# [ ] loops
+
+
+# for(var x = 0; x < 256; x++){
+  # console.log(x)
+# }
+# for x in range(0, 256, 1):
+for x in range(256):
+    print(x) #??
