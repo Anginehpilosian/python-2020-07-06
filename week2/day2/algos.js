ab0626a654ad8ece3011b016a198f305851a6c43
Chris Bautista <CBaut@users.noreply.github.com>
2020-07-14 09:04:27 -0700

    algos

---
+++ week2/day2/algos.js
@@ -0,0 +1,33 @@
+/*
+  String: Dedupe
+
+  Remove duplicate characters
+    - (case-sensitive)
+
+  Bonus: Keep only the last instance of each character.
+*/
+
+const test1 = "abcABC"
+// Output: "abcABC"
+
+const test2 = "helloo"
+// Output: "helo"
+
+// ************************************
+
+/*
+  Reverse words
+  Given a string containing space separated words
+  Reverse each word in the string.
+
+  If you need to, use .split to start, then try to do it without.
+*/
+
+const test1 = "hello"
+// Output: "olleh"
+
+const test2 = "hello world"
+// Output: "olleh dlrow"
+
+const test3 = "abc def ghi"
+// Output: "cba fed ihg"
